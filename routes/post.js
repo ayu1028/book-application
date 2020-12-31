1c7a16c1f9dc2747e79ed185109d11c1466e2284
Ayumi <ayumiatmtlab@gmail.com>
2020-12-31 17:10:49 +0900

    post changed

--- routes/post.js
+++ routes/post.js
@@ -3,6 +3,16 @@ const router = express.Router();
 const db = require('../models/index');
 const multer = require('multer');
 const upload = multer({dest: './public/images/uploads'});
+const cloudinary = require('cloudinary').v2;
+
+require('dotenv').config();
+const env = process.env;
+
+cloudinary.config({
+	cloud_name: env.NAME,
+	api_key: env.KEY,
+	api_secret: env.SECRET_KEY
+});
 
 router.get('/', (req, res, next) => {
 	const data = {
@@ -12,8 +22,7 @@ router.get('/', (req, res, next) => {
 });
 
 router.post('/', upload.single('book_img'), (req, res, next) => {
-	const data = req.body;
-	console.log(data);
+	console.log(req.body);
 	console.log(req.file);
 	res.redirect('/');
 });
